df66b1a9972cd287f4ca02be4e26154d2bf64081
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-06-14 17:24:55 +0700

    Added collision detection

--- car.js
+++ car.js
@@ -13,6 +13,9 @@ class Car{
         this.friction = 0.05;
         this.angle = 0;
 
+        //Check car if crashed or not
+        this.damaged = false;
+
         //Car's sensors
         this.sensor = new Sensor(this);
 
@@ -21,8 +24,54 @@ class Car{
     }
 
     update(roadBorders){
-        this.#move();
+        if(!this.damaged){ // If the car hasn't crashed, we can keep moving the car
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders);
+        }
         this.sensor.update(roadBorders);
+    }
+
+    // Check if the car is crashed or not
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon, roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Locating and modifying the car's corner points
+    #createPolygon(){
+        const points = [];
+        const rad = Math.hypot(this.width, this.height)/2; //The radius of the car
+        const alpha = Math.atan2(this.width, this.height); //The angle of the car
+        // Location of the top right point of the car
+        points.push({
+            x:this.x - Math.sin(this.angle - alpha) * rad,
+            y:this.y - Math.cos(this.angle - alpha) * rad
+        });
+
+        // Location of the bottom right point of the car
+        points.push({
+            x:this.x - Math.sin(this.angle + alpha) * rad,
+            y:this.y - Math.cos(this.angle + alpha) * rad
+        });
+        
+        // Location of the bottom left point of the car
+        points.push({
+            x:this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y:this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        });
+
+        // Location of the top left point of the car
+        points.push({
+            x:this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y:this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        });
+        
+        return points;
     }
 
     #move(){ //Make the movement function private
@@ -80,21 +129,19 @@ class Car{
 
     // This method draws the car on the canvas.
     draw(ctx){
-        // Making the car rotate    
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-
+        // Check if car is crashed or not
+        if(this.damaged){
+            ctx.fillStyle = 'gray';
+        }
+        else{
+            ctx.fillStyle = 'black';
+        }
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2, 
-            this.width, 
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for(let i = 1; i < this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
-
-        ctx.restore();
 
         this.sensor.draw(ctx);
     }
